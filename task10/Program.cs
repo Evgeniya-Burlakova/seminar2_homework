676f74c3409d8fe43871234f66381d0290b79cef
Evgeniya Burlakova <evgenija@211.ru>
2023-07-03 13:40:10 +0700

    Add task 10

---
+++ task10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+Console.Clear();
+Console.Write("Введите трехзначное положительное число: ");
+int n = int.Parse(Console.ReadLine());
+while (n < 100 || n > 999) {
+    Console.WriteLine ("Это число не трехзначное положительное");
+    Console.Write("Введите трехзначное положительное число: ");
+    n = int.Parse(Console.ReadLine());
+}
+int secondNumber = n / 10 % 10;
+Console.WriteLine ($"Вторая цифра числа {n} --> {secondNumber}");
